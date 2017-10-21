3e070a65bfede89927d99df82317e2a3f260ffa1
yahiafarghaly <yahiacommunication@yahoo.com>
2017-10-21 15:05:50 +0200

    Documented the breakpoint class

--- src/breakpoint.cpp
+++ src/breakpoint.cpp
@@ -1,20 +1,48 @@
 #include "breakpoint.h"
 
+/** 
+ *  @brief     Enable setting a breakpoint at a specific address [m_addr] of
+ *              process [m_pid].
+ * 
+ *  @details    In x86, Overwriting an instruction at a specific address by
+ *              int3 instruction(opcode = 0xcc) will trigger the CPU to execute
+ *              a handler in CPU interrupt vector table which the OS - in case of linux -
+ *              pass it the process as a SIGTRAP.
+ *              It is one byte long instruction which makes it perfect to be caught by ptrace.
+ * 
+ *  @Note       Works only for x86 processors.
+ *  @return     void
+ */
 void breakpoint::enable() {
-	// for x86
+	// Fetch the program instruction at the desired address of a specific process.
     auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
-    m_saved_data = static_cast<uint8_t>(data & 0xff); //save bottom byte
+    // Save the lower byte which will be replaced with INT3 instruction.
+    m_saved_data = static_cast<uint8_t>(data & 0xff);
     uint64_t int3 = 0xcc;
-    uint64_t data_with_int3 = ((data & ~0xff) | int3); //set bottom byte to 0xcc
+    // Set the lower byte to INT3 instruction
+    uint64_t data_with_int3 = ((data & ~0xff) | int3);
+    // Push the modified instruction with the breakpoint to the same address it was fetched.
     ptrace(PTRACE_POKEDATA, m_pid, m_addr, data_with_int3);
-
+    // Enable that (this) object of the class has a breakpoint at [m_addr] of [m_pid] process.
     m_enabled = true;
 }
 
+/** 
+ *  @brief     Disable setting a breakpoint at a specific address [m_addr] of
+ *              process [m_pid].
+ * 
+ *  @details    Restore the old lower byte of the instruction instead of INT3 instruction.
+ * 
+ *  @Note       Works only for x86 processors.
+ *  @return     void
+ */
 void breakpoint::disable() {
+    // Fetch the INT3 instruction.
     auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+    // Restore the old lower byte.
     auto restored_data = ((data & ~0xff) | m_saved_data);
+    // Push the old instruction without 0xcc byte.
     ptrace(PTRACE_POKEDATA, m_pid, m_addr, restored_data);
-
+    // Disbale that (this) object of the class has a breakpoint at [m_addr] of [m_pid] process.
     m_enabled = false;
 }
--- src/breakpoint.h
+++ src/breakpoint.h
@@ -14,22 +14,33 @@
 
 class breakpoint {
 public:
-	breakpoint(){}
+    // removing the default constructor causes compilation error ?.
+    breakpoint(){}
+    // Paramterized constructor with address of breakpoint [addr] at a process [pid].
     breakpoint(pid_t pid, std::intptr_t addr)
         : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_saved_data{}
     {}
-	
+    
+    // Enable setting a breakpoint at a specific address [m_addr] of process [m_pid].
     void enable();
+    // Disable setting a breakpoint at a specific address [m_addr] of process [m_pid].
     void disable();
 
+    // is (this) object of the class has an active breakpoint.
     auto is_enabled() const -> bool { return m_enabled; }
+    // return the address of the breakpoint.
     auto get_address() const -> std::intptr_t { return m_addr; }
 
 private:
+    // pid of the process which has a breakpoint.
     pid_t m_pid;
+    // the address which the process has a breakpoint at.
     std::intptr_t m_addr;
+    // is (this) object of the class has an active breakpoint.
     bool m_enabled;
-    uint8_t m_saved_data; //data which used to be at the breakpoint address
+    // the lower byte of the instruction at address [m_addr] which is replaced
+    // with INT3 byte for making a breakpoint in x86 processors.
+    uint8_t m_saved_data;
 };
 
 #endif
